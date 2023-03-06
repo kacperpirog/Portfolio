178ba1f98421f85ea7dcde5915f6c62acffa3aa9
Kacper Pirog <kacper.pirog@gmail.com>
2023-03-06 18:18:55 +0100

    add cv pdf

--- src/components/sliderHome/StyledSlider.js
+++ src/components/sliderHome/StyledSlider.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import profileFoto_02 from "../../assets/images/profile/foto_KP_02.jpg";
 import profileFoto_01 from "../../assets/images/profile/foto_KP_01.jpg";
 import { devices } from "../../styles/globalStyles/GlobalStyles";
+import Modal from "react-modal";
 
 export const StyledSection = styled.section`
   display: flex;
@@ -130,5 +131,55 @@ export const StyleH2 = styled.h2`
     100% {
       transform: scale3d(1, 1, 1);
     }
+  }
+`;
+
+export const ModalWrapper = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  button {
+    margin: 12px;
+    background: linear-gradient(
+      90deg,
+      rgba(153, 90, 223, 1) 50%,
+      rgba(24, 143, 163, 1) 100%
+    );
+    border-radius: 10px;
+    box-shadow: 1px 1px 5px rgba(153, 90, 223, 0.4),
+      -1px -1px 5px rgba(19, 158, 179, 0.4);
+    color: white;
+    display: inline-block;
+    padding: 10px;
+    margin-right: 10px;
+    background-color: #fff;
+    border-radius: 25px;
+    :hover {
+      cursor: pointer;
+      background: #000;
+      color: 1px 1px 5px rgba(153, 90, 223, 0.4);
+    }
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin: auto;
+    background: linear-gradient(#0f1724, #0f1724);
+    border-radius: 8px;
+    padding: 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
   }
 `;
--- src/components/sliderHome/index.js
+++ src/components/sliderHome/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-awesome-slider/dist/styles.css";
 import {
+  ModalWrapper,
   StyledDiv,
   StyledDivCircle,
   StyledSection,
@@ -9,8 +10,16 @@ import {
 import { textHome } from "../../localData";
 import { BtnShowProject } from "../../views/Work/StyledWork";
 import { StyledAnimationsDiv } from "../../views/MySkills/StyledMySkills";
+import image_cv from "../../assets/cv_kacper_pirog_JFD_EN_web.jpg";
+import image_cv_pdf from "../../assets/cv_kacper_pirog_JFD_EN_web.pdf";
 
 const SliderHome = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   const setUpSpaceInText = (id, index) => {
     if (
       (id === 2 && index === 3) ||
@@ -20,125 +29,148 @@ const SliderHome = () => {
       return "\xa0";
     }
   };
-  const openInNewTab = () => {
-    const url = "../../assets/cv_kacper_pirog_JFD_EN_web.pdf"; // zastąpić właściwym adresem URL
-    window.open(url, "_blank");
-  };
+
+  // const openInNewTab = () => {
+  //   const url = "../../assets/cv_kacper_pirog_JFD_EN_web.jpg";
+  //   window.open(url, "_blank", "noopener,noreferrer");
+  // };
   return (
-    <StyledSection>
-      <StyledDiv>
+    <>
+      <StyledSection>
+        <StyledDiv>
+          <div>
+            {textHome.map(({ textHome, id }) => {
+              return (
+                <ul>
+                  <li key={id}>
+                    <>
+                      {textHome.split("").map((word, index) => (
+                        <StyleH2>
+                          {setUpSpaceInText(id, index)}
+                          {word}
+                        </StyleH2>
+                      ))}
+                    </>
+                  </li>
+                </ul>
+              );
+            })}
+          </div>
+          <div>
+            <h3>
+              As a Frontend Developer with a Graphic Designer background,
+              <br /> I possess the knowledge and skills necessary to design and
+              create responsive and visually appealing websites. By combining my
+              knowledge in graphic design and programming, I am able to create
+              websites that blend aesthetic beauty with functionality and ease
+              of use.
+            </h3>
+            <BtnShowProject onClick={() => setModalIsOpen(true)}>
+              <h6>Show CV</h6>
+            </BtnShowProject>
+          </div>
+          <StyledDivCircle>
+            <StyledAnimationsDiv>
+              <div class="container">
+                <svg viewBox="0 0 120 120">
+                  <defs>
+                    <g id="point" class="point-wrapper">
+                      <circle
+                        class="point"
+                        r="0.75"
+                        stroke="white"
+                        fill="white"
+                      />
+                      <circle
+                        class="outline"
+                        r="2"
+                        stroke="white"
+                        fill="none"
+                      />
+                    </g>
+                  </defs>
+                  <circle class="ring" r="59" cx="60" cy="60" stroke="white" />
+                  <circle class="ring" r="40" cx="60" cy="60" stroke="white" />
+                  <circle class="ring" r="20" cx="60" cy="60" stroke="white" />
+                  <g class="wrap" pointer-events="all">
+                    <g class="point-group">
+                      <g class="skill" transform="translate(65, 46)">
+                        <i x="0%" y="5%" className="fas fa-globe-europe" />
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(40, 40)">
+                        <text x="0%" y="5%">
+                          Typescript
+                        </text>
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(65, 69)">
+                        <text x="0%" y="5%">
+                          Javascript
+                        </text>
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(55, 65)">
+                        <text x="0%" y="5%">
+                          CSS
+                        </text>
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(57, 54)">
+                        <text x="0%" y="5%">
+                          HTML
+                        </text>
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(90, 100)">
+                        <text x="0%" y="5%">
+                          React
+                        </text>
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(22, 70)">
+                        <text x="0%" y="5%">
+                          API
+                        </text>
+                      </g>
+                    </g>
+                    <g class="point-group">
+                      <g class="skill" transform="translate(100, 70)">
+                        <text x="0%" y="5%">
+                          Bootstarp
+                        </text>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </StyledAnimationsDiv>
+            <div className="divStyled"></div>
+          </StyledDivCircle>
+        </StyledDiv>
+      </StyledSection>
+      <ModalWrapper
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Przykładowy popup"
+        ariaHideApp={false}
+      >
         <div>
-          {textHome.map(({ textHome, id }) => {
-            return (
-              <ul>
-                <li key={id}>
-                  <>
-                    {textHome.split("").map((word, index) => (
-                      <StyleH2>
-                        {setUpSpaceInText(id, index)}
-                        {word}
-                      </StyleH2>
-                    ))}
-                  </>
-                </li>
-              </ul>
-            );
-          })}
+          <button onClick={closeModal}>
+            <h6>X</h6>
+          </button>
+          <a href={image_cv_pdf} download>
+            <img src={image_cv} alt="Opis obrazu" />
+          </a>
         </div>
-        <div>
-          <h3>
-            As a Frontend Developer with a Graphic Designer background,
-            <br /> I possess the knowledge and skills necessary to design and
-            create responsive and visually appealing websites. By combining my
-            knowledge in graphic design and programming, I am able to create
-            websites that blend aesthetic beauty with functionality and ease of
-            use.
-          </h3>
-          <BtnShowProject onClick={() => openInNewTab()}>
-            <h6>Download CV</h6>
-          </BtnShowProject>
-        </div>
-        <StyledDivCircle>
-          <StyledAnimationsDiv>
-            <div class="container">
-              <svg viewBox="0 0 120 120">
-                <defs>
-                  <g id="point" class="point-wrapper">
-                    <circle
-                      class="point"
-                      r="0.75"
-                      stroke="white"
-                      fill="white"
-                    />
-                    <circle class="outline" r="2" stroke="white" fill="none" />
-                  </g>
-                </defs>
-                <circle class="ring" r="59" cx="60" cy="60" stroke="white" />
-                <circle class="ring" r="40" cx="60" cy="60" stroke="white" />
-                <circle class="ring" r="20" cx="60" cy="60" stroke="white" />
-                <g class="wrap" pointer-events="all">
-                  <g class="point-group">
-                    <g class="skill" transform="translate(65, 46)">
-                      <i x="0%" y="5%" className="fas fa-globe-europe" />
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(40, 40)">
-                      <text x="0%" y="5%">
-                        Typescript
-                      </text>
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(65, 69)">
-                      <text x="0%" y="5%">
-                        Javascript
-                      </text>
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(55, 65)">
-                      <text x="0%" y="5%">
-                        CSS
-                      </text>
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(57, 54)">
-                      <text x="0%" y="5%">
-                        HTML
-                      </text>
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(90, 100)">
-                      <text x="0%" y="5%">
-                        React
-                      </text>
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(22, 70)">
-                      <text x="0%" y="5%">
-                        API
-                      </text>
-                    </g>
-                  </g>
-                  <g class="point-group">
-                    <g class="skill" transform="translate(100, 70)">
-                      <text x="0%" y="5%">
-                        Bootstarp
-                      </text>
-                    </g>
-                  </g>
-                </g>
-              </svg>
-            </div>
-          </StyledAnimationsDiv>
-          <div className="divStyled"></div>
-        </StyledDivCircle>
-      </StyledDiv>
-    </StyledSection>
+      </ModalWrapper>
+    </>
   );
 };
 
